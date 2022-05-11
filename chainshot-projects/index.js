2a3eaa4892ede2f0c2cc229f7375b7e369a6a45a
Derek Lee <derek@Dereks-MacBook-Air-2.local>
2022-05-10 21:49:17 -0400

    add some json rpc calls for practice

---
+++ chainshot-projects/index.js
@@ -0,0 +1,38 @@
+// This index.js code makes a batched JSON RPC call using the Alchemy APIs to read from the ethereum mainnet.
+// The two methods I've used here are: (1) getting the metadata of a certain block, specifically block 436, and (2)
+// getting the ETH balance of the latest block.
+
+// To run this, use "npm install axios" then run "node index.js"
+
+
+const axios = require('axios');
+
+// copy-paste your URL from Alchemy
+const ALCHEMY_URL = "https://eth-mainnet.alchemyapi.io/v2/fXikIvaYIRxC6pxkf7Kd-4f-h93Z1TDF";
+
+async function main() {
+  const response = await axios.post(ALCHEMY_URL, [
+  {
+  jsonrpc: "2.0",
+  id: 1,
+  method: "eth_getBlockByNumber",
+  params: [
+    '0x1b4',
+    true
+  ]
+  },
+  {jsonrpc: "2.0",
+  id: 2,
+  method: "eth_getBalance",
+  params: [
+    '0xe6a7a1d47ff21b6321162aea7c6cb457d5476bca',
+    'latest'
+  ]
+  }])
+
+for (let i = 0; i < response.data.length; i++) {
+  console.log(response.data[i].result)
+}
+};
+
+main();
